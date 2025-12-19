ba6c056d9dd12524eb472f319f8bcf8843be1192
Gowshekan <gowshekangowshekan@gmail.com>
2025-12-19 12:18:30 +0530

    Changes Done

--- bookingsystem/src/config.js
+++ bookingsystem/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://booking-system-backend-1-vmn5.onrender.com/api/v1";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://booking-system-backend-1-vmn5.onrender.com/api/v1";
